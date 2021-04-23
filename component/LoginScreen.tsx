e9a9a712ee17b1ffb3c9915e22ac7b36d14569ba
Anderson Beauvais <e1061615@cmaisonneuve.qc.ca>
2021-04-23 03:12:14 -0400

    Update LoginScreen.tsx

--- component/LoginScreen.tsx
+++ component/LoginScreen.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
-import{Container,Image,Form,Item,Label,Icon,Content,Input,Button}  from 'native-base'
+import { Text, View,Image } from 'react-native'
+import{Container,Form,Item,Label,Icon,Content,Input,Button}  from 'native-base'
 import {AuthContext} from '../context/AuthProvider'
 import { StackNavigationProp } from '@react-navigation/stack';
 import { NavigationStackProp } from 'react-navigation-stack';
@@ -24,12 +24,22 @@ export default class LoginScreen extends Component<Props> {
     render() {
         const { navigation } = this.props;
         const{register,login}=this.context
+        const UrlImg:string = 'https://i.ibb.co/GHtvyKY/logo-health-food-eating-vector-fork-logo-46ef4e30c07292c0988b786f43ea8a52.png'
         return (
                
     <Container >
      
     <Content style={{flex:6}} contentContainerStyle={{flex:6}} scrollEnabled={false}>
- 
+    <View style={{ flexDirection: 'row', flex: 2, justifyContent: "center",marginTop:35 }}>
+          <Image
+            source={{
+              uri:'https://i.ibb.co/GHtvyKY/logo-health-food-eating-vector-fork-logo-46ef4e30c07292c0988b786f43ea8a52.png',
+            }}
+            style={{ width: 125, height: 200 }}
+          />
+
+        </View>
+        <View style={{flex:3 ,flexDirection:'column',justifyContent:'center'}}>
       <Form style={{marginTop:25}}>
         <Item rounded last floatingLabel>
           <Icon type="FontAwesome" name='envelope' style={{ color: 'black' }} />
@@ -43,7 +53,13 @@ export default class LoginScreen extends Component<Props> {
           <Input  onChangeText={password=>this.setState({password:password})} />
         </Item>
       </Form>
-      
+      </View>
+      <View style={{ flex:2, flexDirection:'column',justifyContent:'center'}}>
+        <Button rounded success onPress={()=>{ login(this.state.email,this.state.password)
+          
+        }} block ><Text >Connection</Text></Button>
+        <Text onPress={()=>navigation.navigate('SubcribeScreen')} style={{textAlign:'center',marginTop:15}}>Subcribe here </Text>
+        </View>
     </Content>
   </Container>
         )
